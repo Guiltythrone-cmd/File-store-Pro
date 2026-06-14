3a4f5b2a26ef0d276b3bd35bcdf8d0d56910d3b7
Guiltythrone-cmd <suiiiiiiiiiiiiiiiui@gmail.com>
2026-06-14 15:15:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ DB_NAME = "HypoFlix-Filestore"
 
 FSUBS = [[-1003819872917, True, 10]] # Force Subscription Channels [channel_id, request_enabled, timer_in_minutes]
 # Database Channel (Primary)
-DB_CHANNEL =  -100362807302  # just put channel id dont add ""
+DB_CHANNEL =  -1003628073027  # just put channel id dont add ""
 # Multiple Database Channels (can be set via bot settings)
 # DB_CHANNELS = {
 #     "-1002595092736": {"name": "Primary DB", "is_primary": True, "is_active": True},
